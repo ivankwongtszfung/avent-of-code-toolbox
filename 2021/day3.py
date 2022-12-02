63b930b45d682109d1f0ed7ca68753c5cdf1e1f6
Ivan Kwong <ikwong@waveapps.com>
2022-12-02 00:48:31 -0500

    refactor 2021 day3

--- 2021/day3.py
+++ 2021/day3.py
@@ -2,46 +2,26 @@ from typing import List
 
 
 def solution(lines: List[str]):
-    length = len(lines)
-    digits = len(lines[0])
-    results = [0] * digits
-    gamma = 0
-    epsilon = 0
     nums = get_str_input(lines)
+    o2, co2 = get_result(nums), get_result(nums, mcb=False)
+    return int(o2, 2) * int(co2, 2)
 
-    # for idx, result in enumerate(results):
-    #     print(result, len(results) - idx)
-    #     if result > length // 2:
-    #         gamma += 1 << (len(results) - idx - 1)
-    #     else:
-    #         epsilon += 1 << (len(results) - idx - 1)
-    o2, co2 = get_result(nums, 0), get_result_rev(nums, 0)
-    return int(o2,2) * int(co2,2)
-
-def get_result(nums: List[str], digit: int):
-    if len(nums) == 1:
-        return nums[0]
-    pos = get_position_result(nums)
-    if pos[digit] == len(nums)/ 2: # 1 is the MCB
-        return get_result(get_filtered_result(nums, digit, "1"), digit+1)
-    elif pos[digit] >= len(nums)/ 2: # 1 is the MCB
-        return get_result(get_filtered_result(nums, digit, "1"), digit+1)
-    else: # 0 is the MCB
-        return get_result(get_filtered_result(nums, digit, "0"), digit+1)
-
-def get_result_rev(nums: List[str], digit: int):
-    if len(nums) == 1:
-        return nums[0]
-    pos = get_position_result(nums)
-    if pos[digit] == len(nums)/ 2: # 1 is the MCB
-        return get_result_rev(get_filtered_result(nums, digit, "0"), digit+1)
-    elif pos[digit] >= len(nums)/ 2: # 1 is the MCB
-        return get_result_rev(get_filtered_result(nums, digit, "0"), digit+1)
-    else: # 0 is the MCB
-        return get_result_rev(get_filtered_result(nums, digit, "1"), digit+1)
 
 def get_str_input(lines: List[str]):
     return [str(line, "utf-8") if isinstance(line, bytes) else line for line in lines]
+
+
+def get_result(nums: List[str], digit: int = 0, mcb: bool = True):
+    if len(nums) == 1:
+        return nums[0]
+    pos = get_position_result(nums)
+    if pos[digit] >= len(nums) / 2:  # 1 is the MCB
+        target = "1" if mcb else "0"
+        return get_result(get_filtered_result(nums, digit, target), digit + 1, mcb)
+    else:  # 0 is the MCB
+        target = "0" if mcb else "1"
+        return get_result(get_filtered_result(nums, digit, target), digit + 1, mcb)
+
 
 def get_position_result(nums: List[str]):
     results = [0] * len(nums[0])
@@ -51,8 +31,10 @@ def get_position_result(nums: List[str]):
             results[pos] += 1
     return results
 
+
 def get_filtered_result(nums: List[str], digit: int, target: str):
     return [num for num in nums if num[digit] == target]
+
 
 def test():
     questions = [
